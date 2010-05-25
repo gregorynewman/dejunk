1f500b22dda2f91b17712210d5f900497e8fd19c
Gregory Newman <gregory.e.newman@gmail.com>
2010-05-25 13:34:32 -0700

    added dejunk_until_end, wrote README

--- dejunk.py
+++ dejunk.py
@@ -22,8 +22,8 @@ junk_patterns = [
             'fillwith':[0x90, 0xEB],
         },
         {
-            'pattern':[0x36,0xEB,0x01,'??'],
-            'fillwith':[0x90,0x90,0x90,0x90],
+            'pattern':[0x36,0xEB],
+            'fillwith':[0x90,0xEB],
         },
         {
             'pattern':[0x3E, 0xEB],
@@ -39,16 +39,21 @@ junk_patterns = [
         }, 
     ]
 
+end_patterns = [
+        [0xC3],
+        [0xC2,'??','??'],
+    ]
+
 
 def match_pattern(ea):
 
     for pattern in junk_patterns:
-        # create blank opcode tuple for comparison
+        # create blank opcode list for comparison
         opcode_bytes = []
         opcode_iterator = ea
         for byte in pattern['pattern']:
             if byte == '??':
-                opcode_bytes.append(byte)
+                opcode_bytes.append(byte) # wildcard byte doesn't mater so append wildcard for matching
                 continue
             opcode_bytes.append(get_byte(opcode_iterator))
             opcode_iterator += 1
@@ -57,13 +62,37 @@ def match_pattern(ea):
 
     return None
 
+def match_end(ea):
+    for pattern in end_patterns:
+        opcode_bytes = []
+        opcode_iterator = ea
+        for byte in pattern:
+            if byte == '??':
+                opcode_bytes.append(byte) # wildcard byte doesn't matter so append wildcard for matching
+                continue
+            opcode_bytes.append(get_byte(opcode_iterator))
+            opcode_iterator += 1
+        if opcode_bytes == pattern:
+            return True
+
+    return False
+
 def patch_db(ea, pattern):
     opcode_iterator = ea
     for byte in pattern['fillwith']:
         put_byte(opcode_iterator, byte)
         opcode_iterator += 1
 
+# max_size is set to 1024 to avoid excessive dejunking or not finding any end
+def find_end(ea, max_size=0x400):
+    iterator = ea
+    ea_end = ea+max_size
+    while iterator != 0xFFFFFFFF:
+        if match_end(iterator) == True:
+            return iterator
+        iterator = next_head(iterator, ea_end)
 
+    return False
 
 
 def dejunk_selection():
@@ -72,8 +101,16 @@ def dejunk_selection():
     selection_size = selection_end - selection_start
     print "Dejunking %X - %X" % (selection_start, selection_end)
     dejunk(selection_start, selection_end)
-    do_unknown_range(selection_start, selection_size, DOUNK_SIMPLE)
-    analyze_area(selection_start, selection_end)
+    do_unknown_range(selection_start, selection_size, DOUNK_SIMPLE) # un-analyze code
+    auto_make_code(selection_start, selection_end) # re-analyze to reflect patches
+
+def dejunk_until_end():
+    ea_start = get_screen_ea()
+    ea_end = find_end(ea_start)
+    print "Dejunking %X - %X" % (ea_start, ea_end)
+    dejunk(ea_start, ea_end)
+    do_unknown_range(ea_start, ea_end, DOUNK_SIMPLE) # un-analyze code
+    auto_make_code(ea_start, ea_end) # re-analyze to reflect patches
     
 
 def dejunk(ea_start, ea_end):
